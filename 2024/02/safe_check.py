a3f1726b819606a38e9be3143f93769d11fa0dfa
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-02 12:10:12 -0500

    WIP: I misunderstood the directions in part 2

--- 2024/02/safe_check.py
+++ 2024/02/safe_check.py
@@ -12,31 +12,43 @@ def get_data(filename):
 def is_safe(row):
     prev = None
     prev_diff = None
-    #print(row)
+    safe_tracker = []
+    # print(row)
     for x in row:
         if not prev:
             prev= x
             continue
         diff = x - prev
-        #print(f"{x} - {prev} = {diff}")
+        # print(f"{x} - {prev} = {diff}")
         prev = x
         if not prev_diff:
             prev_diff = diff
         if diff * prev_diff < 0:
             #print("unsafe (diff trend)")
-            return False
+            safe_tracker.append(False)
+            continue
         diff = abs(diff)
         if diff == 0 or diff > 3:
-            #print("unsafe")
-            return False
-    return True
+            # print("unsafe")
+            safe_tracker.append(False)
+            continue
+        safe_tracker.append(True)
+    output = True if safe_tracker.count(False) == 0 else False
+    return output, safe_tracker
 
 def main():
     data = []
     filename = sys.argv[1]
     data = get_data(filename)
-    output = [1 for row in data if is_safe(row)]
+    output = []
+    safe_tracker=[]
+    for row in data:
+        s, s_t = is_safe(row)
+        output.append(1 if s else 0)
+        safe_tracker.append(s_t)
     print(f"Safe reports: {sum(output)}")
+    print(safe_tracker)
+    print(f"Potentially safe: {sum([1 for x in safe_tracker if x.count(False)<2])}")
 
 
 
