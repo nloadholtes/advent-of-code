2e7172c91c079a8fdce8549e2602138852eac70c
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-03 12:32:49 -0500

    Another regex magic trick. I did this one because I know the pipe is used to do an or between patterns. Thankfully this preseves the order the patterns are seen in the ouput. But I will say that I'm surprised that the output from findall went from 2 columns to 5. I need to learn more about that.

--- 2024/03/corrupt_find.py
+++ 2024/03/corrupt_find.py
@@ -8,6 +8,7 @@ import re
 
 
 REGEX_PATTERN = r"mul\((\d+),(\d+)\)"
+REGEX_PATTERN2 = r"(mul\((\d+),(\d+)\))|(don't\(\))|(do\(\))"
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -21,3 +22,22 @@ if __name__ == "__main__":
     for x in muls:
         output.append(int(x[0]) * int(x[1]))
     print(f"Sum of muiltiplications is: {sum(output)}")
+    print("************ part 2 *************")
+    muls = re.findall(REGEX_PATTERN2, data)
+    skip = False # True  # Pays to read the instructions. It starts assuming muls are enabled.
+    counter = 0
+    output = []
+    for x in muls:
+        if "do()" in x[4]:
+            skip = False
+        if "don't()" in x[3]:
+            skip = True
+        if skip:
+            continue
+        if x[1] and x[2]:
+            output.append(int(x[1]) * int(x[2]))
+        counter += 1
+        # breakpoint()
+        # if counter > 40:
+        #    break
+    print(f"Sum of multiplications is: {sum(output)}")
