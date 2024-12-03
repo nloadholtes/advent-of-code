3d343e968c9ae49e566b42472afae7df810fc57d
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-03 10:18:22 -0500

    This feels like a step in the right direction, but the answer of 442 was too low

--- 2024/02/safe_check.py
+++ 2024/02/safe_check.py
@@ -9,10 +9,11 @@ def get_data(filename):
             data.append([int(x) for x in line.split()])
     return data
 
-def is_safe(row):
+def is_safe(row, tolerance=0):
     prev = None
     prev_diff = None
     safe_tracker = []
+    tolerance_count = 0
     # print(row)
     for x in row:
         if not prev:
@@ -20,35 +21,39 @@ def is_safe(row):
             continue
         diff = x - prev
         # print(f"{x} - {prev} = {diff}")
-        prev = x
         if not prev_diff:
             prev_diff = diff
         if diff * prev_diff < 0:
-            #print("unsafe (diff trend)")
+            # print("unsafe (diff trend)")
+            if tolerance_count < tolerance:
+                tolerance_count += 1
+                continue
             safe_tracker.append(False)
+            prev = x
             continue
         diff = abs(diff)
         if diff == 0 or diff > 3:
-            # print("unsafe")
+            #print("unsafe" )
+            if tolerance_count < tolerance:
+                tolerance_count += 1
+                continue
             safe_tracker.append(False)
+            prev = x
             continue
+        prev = x
         safe_tracker.append(True)
     output = True if safe_tracker.count(False) == 0 else False
-    return output, safe_tracker
+    return output  # , safe_tracker
 
 def main():
     data = []
     filename = sys.argv[1]
     data = get_data(filename)
-    output = []
-    safe_tracker=[]
-    for row in data:
-        s, s_t = is_safe(row)
-        output.append(1 if s else 0)
-        safe_tracker.append(s_t)
-    print(f"Safe reports: {sum(output)}")
-    print(safe_tracker)
-    print(f"Potentially safe: {sum([1 for x in safe_tracker if x.count(False)<2])}")
+    output = [1 for row in data if is_safe(row)]
+    print(f"Safe: {sum(output)}")
+    
+    within_tolerance = [1 for row in data if is_safe(row, tolerance=1)]
+    print(f"Safe within tolerance of 1: {sum(within_tolerance)}")
 
 
 
