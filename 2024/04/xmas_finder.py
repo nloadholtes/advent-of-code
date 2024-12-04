e4dd253c15753ed78f7364d8d7484eaf2241a196
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-04 10:57:59 -0500

    Looking at lines and columns (forward and reverse) and this finds 13 of the 18

--- 2024/04/xmas_finder.py
+++ 2024/04/xmas_finder.py
@@ -4,6 +4,20 @@ import sys
 
 PHRASE = "XMAS"
 
+def count_phrase(lines, phrase=PHRASE):
+    counter = 0
+    def _line_counter(lin, counter):
+        for line in lin:
+            if phrase in line:
+                counter += 1
+            if phrase in ''.join(list(reversed(line))):
+                counter += 1
+        return counter
+    counter += _line_counter(lines, counter)
+    # Transform?
+    transpose = [''.join(col) for col in zip(*lines)]
+    counter += _line_counter(transpose, counter)
+    return counter
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -13,5 +27,5 @@ if __name__ == "__main__":
         for line in f:
             data.append(line)
 
-    
+    print(f"{PHRASE} is seen {count_phrase(data)} times")
 
