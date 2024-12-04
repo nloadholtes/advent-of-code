510d59b0530429c9f969c078855ea6b228a50e93
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-04 17:41:50 -0500

    wip

--- 2024/04/xmas_finder.py
+++ 2024/04/xmas_finder.py
@@ -19,6 +19,23 @@ def count_phrase(lines, phrase=PHRASE):
     counter += _line_counter(transpose, counter)
     return counter
 
+
+def count_phrase_brute(lines, phrase):
+    counter = 0
+    # Recusively call to check the length of the phrase? (e.g. one letter at a time)
+    for row in lines:
+        for col in row:
+            # Scan left for phrase
+            # Scan Down (if room)
+            #
+            for l in phrase:
+                if c != l:
+                    break
+
+
+    return counter
+
+
 if __name__ == "__main__":
     filename = sys.argv[1]
     data = []
