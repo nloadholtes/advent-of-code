a93e79b609562774ec6d709af3ae578b5bffd0d6
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-05 09:47:11 -0500

    Skeleton in place

--- 2024/05/ordering.py
+++ 2024/05/ordering.py
@@ -12,6 +12,10 @@ def create_mapping(rules):
         output[first] = values
     return output
 
+def is_order_correct(mappings, update):
+    for page in update:
+        pass
+    return True
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -20,7 +24,14 @@ if __name__ == "__main__":
         data = f.readlines()
 
     rules = [x.strip() for x in data if "|" in x]
-    updates = [x.strip() for x in data if "|" not in x]
+    updates = [list(map(lambda y: int(y), x.strip().split(","))) for x in data if x != "\n" and "|" not in x]
 
     mappings = create_mapping(rules)
     print(mappings)
+    middle_pages = []
+    for update in updates:
+        if is_order_correct(mappings, update):
+            middle_page = int(len(update)/2)
+            middle_pages.append(update[middle_page])
+    print(f"Sum of middle pages: {sum(middle_pages)}")
+
