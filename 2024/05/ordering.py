289d31cd337ecfb626938fc7efcf54d7ed31c487
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-05 10:29:55 -0500

    This is close. I need to pop more off until I find the spot where value truly fits

--- 2024/05/ordering.py
+++ 2024/05/ordering.py
@@ -22,6 +22,30 @@ def is_order_correct(mappings, update):
                 return False
     return True
 
+def reorder(mappings, update):
+    output = []
+    prev_output = []
+    def _process(mappings, update):
+        output = []
+        for page in update:
+            output.append(page)
+            values = mappings.get(page, [])
+            for value in values:
+                if value in output:
+                    # reorder? Might need to pop until value fits?
+                    last= output.pop()
+                    prev = output.pop()
+                    output.append(last)
+                    output.append(prev)
+        print(f"---{output}")
+        return output
+    output = _process(mappings, update)
+    while prev_output != output:
+        prev_output = output
+        output = _process(mappings, output)
+    print(f"{update}\n{output}")
+    return output
+
 if __name__ == "__main__":
     filename = sys.argv[1]
 
@@ -32,11 +56,17 @@ if __name__ == "__main__":
     updates = [list(map(lambda y: int(y), x.strip().split(","))) for x in data if x != "\n" and "|" not in x]
 
     mappings = create_mapping(rules)
-    print(mappings)
+    #print(mappings)
     middle_pages = []
+    fixup = []
     for update in updates:
         if is_order_correct(mappings, update):
             middle_page = int(len(update)/2)
             middle_pages.append(update[middle_page])
+        else:
+            new_row = reorder(mappings, update)
+            middle_page = int(len(new_row)/2)
+            fixup.append(new_row[middle_page])
+            
     print(f"Sum of middle pages: {sum(middle_pages)}")
-
+    print(f"Sum of fixedup pages: {sum(fixup)}")
