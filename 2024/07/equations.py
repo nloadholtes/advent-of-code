a2faa94b5401f47f15aaad47b6f044ea5bce7723
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-07 23:28:32 -0500

    WIP, not working yet

--- 2024/07/equations.py
+++ 2024/07/equations.py
@@ -1,5 +1,19 @@
 import sys
 
+def is_valid(row):
+    output = False
+    value = row[0]
+    numbers = row[1]
+    total = 1
+    for x in numbers:
+        total *= x
+        if total > value:
+            return False
+
+    if total == value:
+        output = True
+
+    return output
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -10,3 +24,6 @@ if __name__ == "__main__":
             numbers = [int(x) for x in line.split(":")[1].split()]
             data.append([value, numbers])
     print(data)
+    output = []
+    output = [x[0] for x in data if is_valid(x)]
+    print(f"Sum of valid values: {sum(output)}")
