14eae9044c6fad65e0eeb1b1a370f43bb4f857f3
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-09 10:45:26 -0500

    Parses correctly, now to to compact

---
+++ 2024/09/defrag.py
@@ -0,0 +1,37 @@
+import sys
+import pdb
+import sys
+
+def excepthook(type, value, traceback):
+    # Print the exception
+    print(f"Exception caught: {value}")
+    # Start the debugger
+    pdb.post_mortem(traceback)
+
+# Override the default exception handler
+sys.excepthook = excepthook
+
+def generate_fs_map(data):
+    output = ""
+    is_file = -1
+    counter = -1
+    for item in data:
+        count = int(item)
+        counter += 1
+        is_file += 1
+        if is_file == 0:
+            output += "0" * count
+            continue
+        c = "." if is_file % 2 else str(counter)
+        if c == ".":
+            counter -= 1
+        output += c * count
+
+    return output
+
+
+if __name__ == "__main__":
+    data = open(sys.argv[1]).read().strip()
+    fs = generate_fs_map(data)
+    print(fs)
+    print("00...111...2...333.44.5555.6666.777.888899")
