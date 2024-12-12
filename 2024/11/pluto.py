c1cba98830e7fb6ed778adc09c64d75e623cacd8
Nick Loadholtes <nick@ironboundsoftware.com>
2024-12-12 11:57:52 -0500

    Leading 0 will cause the odd-even check to be off

--- 2024/11/pluto.py
+++ 2024/11/pluto.py
@@ -15,15 +15,18 @@ if __name__ == "__main__":
                 #breakpoint()
                 size = int(len(num)/2)
                 tmp_data.append(num[0:size])
-                tmp_data.append(num[size:])
+                tmp_data.append(str(int(num[size:])))
             elif int(num) == 0:
                 tmp_data.append("1")
             else:
                 tmp_data.append(str(2024*int(num)))
 
-        #print(tmp_data)
+        print(f"--len: {len(tmp_data)}")
+        #print(" ".join(tmp_data))
         data = tmp_data
 
     print(f"Length of stones: {len(data)}")
+    print("2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2")
+    #print(" ".join(data))
 
 
