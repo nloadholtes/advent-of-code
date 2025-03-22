4891a579ec818e44ce80a95db112981550c635c4
Nick Loadholtes <nick@ironboundsoftware.com>
2025-03-22 15:15:53 -0400

    WIP, this is from a while ago. Not sure what I was doing, but saving it just in case

--- 2024/19/towels.py
+++ 2024/19/towels.py
@@ -16,7 +16,7 @@ def factor(row, available):
     # If there's no martch return false
     return output
 
-def can_construct(target, available):
+def can_construct(target, available, any_matches=False):
     dp = [False] * (len(target) + 1)
     dp[0] = True  # Empty string is always constructible
     
@@ -26,9 +26,26 @@ def can_construct(target, available):
                 target[i-len(word):i] == word and 
                 dp[i-len(word)]):
                 dp[i] = True
-                break
+                if any_matches is False:
+                    break
     
     return dp[len(target)]
+
+def find_all_constructions(design, patterns):
+    dp = [[] for _ in range(len(design) + 1)]
+    dp[0] = [[]]  # Empty list of patterns is valid for empty string
+    
+    for i in range(1, len(design) + 1):
+        for pattern in patterns:
+            if (i >= len(pattern) and 
+                design[i-len(pattern):i] == pattern and 
+                dp[i-len(pattern)]):
+                # For each valid construction up to i-len(pattern)
+                for prev_construction in dp[i-len(pattern)]:
+                    # Add this pattern to that construction
+                    dp[i].append(prev_construction + [pattern])
+    
+    return dp[len(design)]  # Returns list of all valid pattern combinations
 
 if __name__ == "__main__":
     data = [x.strip() for x in open(sys.argv[1]).readlines()]
@@ -36,5 +53,9 @@ if __name__ == "__main__":
     designs = []
     for row in data[2:]:
         designs.append(can_construct(row, available))
-    print(f"Designs: {designs}")
     print(f"Number of designs we can make: {sum(designs)}")
+    designs = []
+    for row in data[2:]:
+        designs.append(len(find_all_constructions(row, available)))
+    print(designs)
+    print(f"Using any_matches, number of designs we can make: {sum(designs)}")
